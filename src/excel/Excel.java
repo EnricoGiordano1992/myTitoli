da6b18ec8de394b59c4a34d2aa31e38d281ec77d
EnricoGiordano1992 <enricogiordano1992@gmail.com>
2014-06-09 22:26:22 +0200

    finito

--- src/excel/Excel.java
+++ src/excel/Excel.java
@@ -1,13 +1,47 @@
 package excel;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.*;
 
 public class Excel {
 	public static HSSFWorkbook readFile(String filename) throws IOException {
 		return new HSSFWorkbook(new FileInputStream(filename));
+	}
+	
+	public static void modifyValueOfTitoliFile(double value) throws IOException{
+		
+		HSSFWorkbook wb = Excel.readFile("SPREAD BTP.xls");
+		HSSFSheet sheet = wb.getSheetAt(0);
+
+		/*
+		 * primo campo
+		 */
+		HSSFRow row = sheet.getRow(3);
+		HSSFCell cell = row.getCell(5);
+		System.out.println("vecchio valore: " + cell);
+		cell.setCellValue(value);
+		System.out.println("nuovo valore: " + cell);
+
+		/*
+		 * secondo campo
+		 */
+		row = sheet.getRow(17);
+		cell = row.getCell(5);
+		cell.setCellValue(value);
+
+		/*
+		 * terzo campo
+		 */
+		row = sheet.getRow(30);
+		cell = row.getCell(5);
+		cell.setCellValue(value);
+
+		System.out.println("salvataggio...");
+		wb.write(new FileOutputStream("SPREAD BTP.xls"));
+		System.out.println("salvataggio completato");
+		
 	}
 
 }
